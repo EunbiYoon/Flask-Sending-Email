aad0e5e1d1b3763377d1fcdce2df70ed948bfd7e
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-03-02 13:00:19 -0600

    sdfsdf

--- app.py
+++ app.py
@@ -39,5 +39,6 @@ def send_mail():
     # return 
     return render_template("thank.html")
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
